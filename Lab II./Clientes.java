d535c78086dc451b1bc2f6f9e419ec8b91e07f94
Gabriel D'Epiro <90358435+GabrielDepiro@users.noreply.github.com>
2022-03-16 22:42:34 -0300

    Update Clientes.java

--- Lab II./Clientes.java
+++ Lab II./Clientes.java
@@ -10,29 +10,29 @@ public class Clientes {
 	      
 	 public Clientes (String nome, int idade, String email, int telefone){ 
 	   
-		  ​this​.​nome = nome; 
-		  ​this​.​idade = idade; 
-		  ​this​.​email = email; 
-		  ​this​.​telefone = telefone; 
+		  this.nome = nome; 
+		  this.idade = idade; 
+		  this.email = email; 
+		  this.telefone = telefone; 
 	   
 	  } 
 	   
-	  ​public​ ​void​ ​Informacao​(){ 
-		  System​.​out​.​println(​"​Nome: ​"​ ​+​ nome ​+​ ​"​\n​"​ ​+ 
-				  ​"​Idade: ​"​ ​+​ idade ​+​ ​"​\n​"​ ​+ 
-				  ​"​Email: ​"​ ​+​ email ​+​ ​"​\n​"​ ​+ 
-				  ​"​Telefone: ​"​ ​+​ telefone ​+​ ​"​\n​"​ ​+​ ​"​\n​"​); 
-	  ​    } 
+	  public void Informacao(){ 
+		  System.out.println("Nome: " + nome + "\n" + 
+				  "Idade: " + idade + "\n" + 
+				  "Email: " + email + "\n" + 
+				  "Telefone: " + telefone + "\n"); 
+	      } 
 	   
 	   
-	  ​public​ ​static​ ​void​ ​main​(​String​[] ​args​) { 
+	  public static void main(String[] args) { 
 	   
-		  Cliente​ ​C1​ ​=​ ​new​ ​Cliente​(​"​Costa"​, ​29, ​"​costa@gmail.com.br​"​, ​(12)985345671); 
-		  ​Cliente​ ​C2​ ​=​ ​new​ ​Cliente​(​"​Evangelista"​, ​22, ​"​evangelista@yahoo.com​"​, ​(12)989547879); 
+		  Cliente C1 = new Cliente("Costa", 29, "costa@gmail.com.br", (12)985345671); 
+		  Cliente C2 = new Cliente("Evangelista", 22, "evangelista@yahoo.com", (12)989547879); 
 	   
-		  C1​.​Informacao(); 
-		  C2​.​Informacao(); 
-	  ​         
-	  ​    } 
+		  C1.Informacao(); 
+		  C2.Informacao(); 
+	           
+	      } 
 }
 
